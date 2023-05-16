ca542202f24555666113bbab18a6a62ca378a5c5
Dinara Aldarova <dinara_aldarova@mail.ru>
2023-05-16 21:44:18 +0500

    Первый комит

--- src/index.js
+++ src/index.js
@@ -4,11 +4,38 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
+import Login from "./routes/login";
+import Register from "./routes/register";
+import User from "./routes/user";
+
+import ErrorPage from "./routes/error-page";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Navigate to="login"/>,
+        errorElement: <ErrorPage/>,
+    },
+    {
+        path: "/login",
+        element: <Login/>,
+    },
+    {
+        path: "/register",
+        element: <Register/>,
+    },
+    {
+        path: "/user",
+        element: <User/>,
+    },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/routes/register.jsx
@@ -0,0 +1,7 @@
+// src/routes/register.jsx
+
+export default function Register() {
+    return (
+        <h1>Register</h1>
+    )
+}
